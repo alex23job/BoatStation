e0d97b751cd56b522bb3666181684e08e1c90a1a
alex23job <alex23job@yandex.ru>
2024-09-26 01:20:05 +0300

    Adding class Boat

---
+++ BoatStation/Boat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoatStation
+{
+    public class Boat
+    {
+        public int BoatID { get; }
+        public string BoatName { get; }
+        public string Description { get; }
+        public string SerialNumber { get; }
+
+        public Boat() { }
+        public Boat(int id, string nm, string descr, string sn)
+        {
+            BoatID = id;
+            BoatName = nm;
+            Description = descr;
+            SerialNumber = sn;
+        }
+    }
+}
